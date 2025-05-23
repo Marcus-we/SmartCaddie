ef5391b6a1c8a62167073a030d2c338cc475cc4b
Marcus-we <marcus.wetterstrom@hotmail.com>
2025-05-23 15:07:15 +0200

    initialized a mobile app frontend using react native + expo + nativewind

---
+++ frontend/app/index.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import '../global.css'
+
+
+function Home() {
+    return (
+    <View className="flex justify-center items-center">        
+        <Text className="text-xl font-bold" >SmartCaddie</Text>
+    </View>
+    )
+};
+
+export default Home;
+
