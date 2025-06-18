892af311e885b5f2416ecffae98693f34a6435f8
Marcus-we <marcus.wetterstrom@hotmail.com>
2025-06-18 15:57:09 +0200

    small change

--- backend/main.py
+++ backend/main.py
@@ -2,16 +2,15 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-# from app.api.v1.core.schemas import
 from app.api.v1.routers import router
 from app.db_setup import init_db
 
 
 # Funktion som körs när vi startar FastAPI -
-# perfekt ställe att skapa en uppkoppling till en databas
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    init_db()  # Vi ska skapa denna funktion
+    init_db() 
     yield
 
 
